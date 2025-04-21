3db4df0413ce6f7119d6768139318807d09c044a
Abhishek Singh <singh302@gmail.com>
2025-04-20 20:25:13 -0500

    Update snowflake_utils.py

--- snowflake_utils.py
+++ snowflake_utils.py
@@ -14,7 +14,14 @@ conn = snowflake.connector.connect(
     )
 
 def run_query(query):
-    
+    conn = snowflake.connector.connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA")
+    )
     cur = conn.cursor()
     cur.execute(query)
     result = cur.fetchall()
